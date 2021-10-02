a61df97ca26a52d73882bb37ff3201b784cc907f
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-01 23:38:17 -0600

    Connect Nav with Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import './Header.css';
 import React from "react";
+import Nav from '../Nav/Nav';
 
 export const Header: React.FC = () => {
   return (
     <div className="header">
       <h1>Suspicious IP</h1>
+      <Nav />  
     </div>
   );
 }
