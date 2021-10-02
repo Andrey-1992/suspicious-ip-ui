91240199747df19c9b8b16af52dfacb3f45585c9
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-02 09:19:05 -0600

    Connect IpSearched with App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import { Header } from '../Header/Header';
 import { Error } from '../Error/Error';
 import { Home } from '../Home/Home';
 import { IpLocal } from '../IpLocal/IpLocal';
+import { IpSearched } from '../IpSearched/IpSearched';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Error />
       <Home />
       <IpLocal />
+      <IpSearched />
     </div>
   );
 }
