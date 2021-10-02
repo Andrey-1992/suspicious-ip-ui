42dd51efdfdab1237152be829e137b9c23155af8
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-01 23:22:10 -0600

    set up type configuration for Header function component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,10 @@
 import './Header.css';
+import React from "react";
 
-function Header() {
+export const Header: React.FC = () => {
   return (
     <div className="header">
       <h1>Suspicious IP</h1>
     </div>
   );
 }
-
-export default Header;
