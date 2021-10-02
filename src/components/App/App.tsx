72dbec809918b47174c8b99e4d22388cb1d11b6d
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-01 23:48:05 -0600

    Connect with App component and set up the types for the Error component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import { Header } from '../Header/Header';
+import { Error } from '../Error/Error';
 
 function App() {
   return (
     <div className="App">
       <Header /> 
+      <Error />
     </div>
   );
 }
