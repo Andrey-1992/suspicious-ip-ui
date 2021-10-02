e9b1c6b478e3e1865f9e2e2d2c7495d94853d1b3
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-01 23:56:01 -0600

    Connect with App component and set up the types for the home component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { Header } from '../Header/Header';
 import { Error } from '../Error/Error';
+import { Home } from '../Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header /> 
       <Error />
+      <Home />
     </div>
   );
 }
