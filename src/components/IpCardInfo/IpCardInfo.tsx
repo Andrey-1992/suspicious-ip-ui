6f746a124782cc8308f6b5815b9ba4af61f2e358
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-02 09:01:11 -0600

    Stablish types for IPLocal component

--- src/components/IpCardInfo/IpCardInfo.tsx
+++ src/components/IpCardInfo/IpCardInfo.tsx
@@ -9,23 +9,24 @@ interface IpInfo {
   region?: string
   region_code?: string
   country_code?: string
-  country_code_iso3?: string
+  country?: string
   country_name?: string
+  continent_code?: string
+  country_code_iso3?: string
   country_capital?: string
   country_tld?: string
-  continent_code?: string
   in_eu?: boolean
   postal?: string
   latitude?: number
   longitude?: number
   timezone?: string
   utc_offset?: string
+  country_calling_code?: string
   currency?: string
   currency_name?: string
   languages?: string
   country_area?: string
   country_population?: string
-  country_calling_code?: string
   asn?: string
   org?: string
 }
--- src/components/IpLocal/IpLocal.tsx
+++ src/components/IpLocal/IpLocal.tsx
@@ -1,10 +1,39 @@
 import './IpLocal.css';
 import React, { useState, useEffect } from 'react';
 import { IpCardInfo } from '../IpCardInfo/IpCardInfo';
-// import { fetchLocalIp } from '../Utils/utils';
+// import { fetchLocalIp } from '../Util/util';
+
+interface IpInfo {
+  ip?: string
+  version?: string
+  city?: string
+  region?: string
+  region_code?: string
+  country_code?: string
+  country?: string
+  country_name?: string
+  continent_code?: string
+  country_code_iso3?: string
+  country_capital?: string
+  country_tld?: string
+  in_eu?: boolean
+  postal?: string
+  latitude?: number
+  longitude?: number
+  timezone?: string
+  utc_offset?: string
+  country_calling_code?: string
+  currency?: string
+  currency_name?: string
+  languages?: string
+  country_area?: string
+  country_population?: string
+  asn?: string
+  org?: string
+}
 
 export const IpLocal: React.FC = () => {
-  // const [requestLocalIp, setRequestLocalIp] = useState({});
+  const [requestLocalIp, setRequestLocalIp] = useState<IpInfo>({});
 
   // useEffect(() => {
   //   getLocalIpInfo()
