b4d679dc7eea300b7f655e6a7cab04ee2665d238
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-05 17:25:19 -0600

    Add routing path into the App component for the multi page view of the app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,25 @@
 import './App.css';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { Error } from '../Error/Error';
 import { Home } from '../Home/Home';
 import { IpLocal } from '../IpLocal/IpLocal';
 import { IpSearched } from '../IpSearched/IpSearched';
 
-function App() {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <Header /> 
-      <Home />
-      <IpLocal />
-      <IpSearched />
-      <Error />
+      <main className="main-section">
+        <Switch>
+          <Route exact path="/suspicious-ip" render={() => <Home />}/>
+          <Route exact path="/local-ip" render={() => <IpLocal />}/>
+          <Route exact path="/searched-ip" render={() => <IpSearched />}/>
+          <Route path="*" render={() => <Error />}/>
+        </Switch>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
